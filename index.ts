b03d2c778bd46fa9a1fe22194985aadd05fe6350
mikemalkhasyan <Mkrtich_Malkhasyan@epam.com>
2021-05-03 18:25:57 +0400

    - use Typescript classes in Node.js

--- index.ts
+++ index.ts
@@ -2,12 +2,16 @@ import * as express from 'express';
 import * as mongoose from 'mongoose';
 import * as bodyParser from 'body-parser';
 import routes from './src/routes/crmRoutes';
+import messanger from './src/controllers/createMessage';
 
 const app = express();
 // number type
 const PORT: number = 3000;
 const mlabUser: string = "Malkhasyan";
 const mlabPass: string = "x43norte";
+
+//instance of our class
+let messages = new messanger(PORT);
 
 const dataConnection = (user: string, pass: string): string => {
     return `mongodb://${user}:${pass}@cluster0-shard-00-00.4vjqo.mongodb.net:27017,cluster0-shard-00-01.4vjqo.mongodb.net:27017,cluster0-shard-00-02.4vjqo.mongodb.net:27017/test_database?ssl=true&replicaSet=atlas-6r2gny-shard-0&authSource=admin&retryWrites=true&w=majority`;
@@ -31,9 +35,9 @@ routes(app);
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-    res.send(`Node and express server is running on port ${PORT}`)
+    res.send(messages.messagePrint())
 );
 
 app.listen(PORT, () =>
-    console.log(`your server is running on port ${PORT}`)
+    console.log(messages.messagePrint())
 );
