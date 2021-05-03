5004a01855abe25ab7cb67f0e0db49b0bb40ad24
mikemalkhasyan <Mkrtich_Malkhasyan@epam.com>
2021-05-03 17:58:23 +0400

    - adding basic types

--- index.ts
+++ index.ts
@@ -4,10 +4,16 @@ import * as bodyParser from 'body-parser';
 import routes from './src/routes/crmRoutes';
 
 const app = express();
-const PORT = 3000;
+// number type
+const PORT: number = 3000;
+
+// string
+const database: string = "mongodb://Malkhasyan:x43norte@cluster0-shard-00-00.4vjqo.mongodb.net:27017,cluster0-shard-00-01.4vjqo.mongodb.net:27017,cluster0-shard-00-02.4vjqo.mongodb.net:27017/test_database?ssl=true&replicaSet=atlas-6r2gny-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+const n: number = 100;
 
 // mongoose connection
-mongoose.connect('mongodb://Malkhasyan:x43norte@cluster0-shard-00-00.4vjqo.mongodb.net:27017,cluster0-shard-00-01.4vjqo.mongodb.net:27017,cluster0-shard-00-02.4vjqo.mongodb.net:27017/test_database?ssl=true&replicaSet=atlas-6r2gny-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect(database, {
     useMongoClient: true,
 });
 
--- src/models/crmModel.ts
+++ src/models/crmModel.ts
@@ -2,7 +2,7 @@ import * as mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-export const ContactSchema = new Schema({
+const schema: any = {
     firstName: {
         type: String,
         required: 'Enter a first name'
@@ -21,7 +21,9 @@ export const ContactSchema = new Schema({
         type: Number
     },
     created_date: {
-       type: Date,
-       default: Date.now 
+        type: Date,
+        default: Date.now
     }
-});
+}
+
+export const ContactSchema = new mongoose.Schema(schema);
