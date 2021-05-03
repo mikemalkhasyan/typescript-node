574c1557783b9d72fd42ea5e2b3d989663a7384e
mikemalkhasyan <Mkrtich_Malkhasyan@epam.com>
2021-05-03 17:30:56 +0400

    - implement ts-node - add tsconfig.json

--- index.ts
+++ index.ts
@@ -1,13 +1,12 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
 import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 3000;
 
 // mongoose connection
-mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://Malkhasyan:x43norte@cluster0-shard-00-00.4vjqo.mongodb.net:27017,cluster0-shard-00-01.4vjqo.mongodb.net:27017,cluster0-shard-00-02.4vjqo.mongodb.net:27017/test_database?ssl=true&replicaSet=atlas-6r2gny-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useMongoClient: true,
 });
