c1548832a83aa474494c8cc961c24d19840b2065
mikemalkhasyan <Mkrtich_Malkhasyan@epam.com>
2021-05-03 18:26:08 +0400

    - use Typescript classes in Node.js

---
+++ src/controllers/createMessage.ts
@@ -0,0 +1,13 @@
+export default class Messnger {
+    port: number;
+
+    constructor(port) {
+        this.port = port;
+
+    }
+
+    messagePrint() {
+        return `Node and express server is running on port ${this.port}`
+    }
+
+}
